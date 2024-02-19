e56644c213a47399f4a97ff94997a0a8f34e7e5b
Sakib <ssar96540@gmail.com>
2024-02-19 20:14:40 +0600

    modify some style and respnsive

--- ss.js
+++ ss.js
@@ -1,13 +1,4 @@
- const fbtn = document.getElementById('buyTickets');
- 
- fbtn.addEventListener('click',function(){
- 
-console.log('clicked');
- });
-
- 
- 
- const mid = 'Economoy';
+  const mid = 'Economoy';
  let tsum = 0;
  let teven = 40;
 let totalPrice = 0;
@@ -15,23 +6,12 @@ let totalPrice = 0;
  for(let index = 0; index<buttons.length;index++){ 
   const button = buttons[index];
   button.addEventListener("click",function(){
-    
-     
-    // const bootto = document.querySelectorAll(".set-btn");
-    // console.log(bootto);
-    // const boto = button.querySelectorAll('.btn');
-    // console.log(boto);
-   
-
-    // button.classList.add('bg-red-600');
+    // button color
     const bgcolor = button.querySelector('button');
-   
     bgcolor.classList.add('text-white');
      bgcolor.classList.add('bg-green-400');
-     
 
     const title = button.querySelector('span').innerText;
-    
     const price =parseInt(document.getElementById("busPrice").innerText);
      totalPrice+= price ;
       console.log(totalPrice);
@@ -51,42 +31,21 @@ let totalPrice = 0;
     const td = document.createElement("h1");
     td.innerText = price;
     busSit.appendChild(td);
-   
     // title count
     tsum = tsum +1;
-   document.getElementById('titleCount').innerText = tsum;
-      
-     
+   document.getElementById('titleCount').innerText = tsum;    
     // seats even
     teven = teven -1;
     document.getElementById('seats-even').innerText=teven;
-    
-   
-   
- 
-    //  document.getElementById('seat').innerText = title;
-  
-    //  document.getElementById('price').innerText = price;
-  
     document.getElementById('uptotal').innerText = totalPrice;
     document.getElementById('grand-total').innerText=totalPrice;
-
   })
-  // console.log(button);
  }
-
-
 //  popup
  const nextBtn = document.getElementById('next-btn');
  const ppp = document.getElementById('popup');
  const titcketeBey = document.getElementById('ticket-buy');
  const closepopup = document.getElementById('closePopup');
-//  function openPopup(){
-//   ppp.classList.remove('hidden')
-//  }
-//  function closePopup(){
-//   ppp.classList.add('hidden')
-//  }
  nextBtn.addEventListener('click',function(){
   titcketeBey.classList.add('hidden')
 ppp.classList.remove('hidden')
@@ -95,10 +54,7 @@ ppp.classList.remove('hidden')
  closepopup.addEventListener('click',function(){
   titcketeBey.classList.remove('hidden')
   ppp.classList.add('hidden')
- })
-
-
-
+ });
 // coupon
  const aplybtn = document.getElementById('applyBtn');
  aplybtn.addEventListener('click',function(){
@@ -110,7 +66,6 @@ ppp.classList.remove('hidden')
       const divided=multiply/100;
       const even=totalPrice-divided;
       document.getElementById('grand-total').innerText=even;
-      console.log(even);
       const co =document.getElementById('coupon-hide');
       co.classList.add('hidden')
      }
@@ -119,11 +74,10 @@ ppp.classList.remove('hidden')
       const divid = multi/100;
       const eve = totalPrice-divid;
       document.getElementById('grand-total').innerText=eve;
-      console.log(eve);
       const co =document.getElementById('coupon-hide');
       co.classList.add('hidden')
      }
      else{
       alert('Invalid coupon')
      }
- })
+ });
